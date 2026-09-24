da02817fef7218de1c9735252c7f19209f1fa1e7
Vimal Chamoli <vimaldchamoli@gmail.com>
2026-09-24 18:26:59 +0530

    add defualt page

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-app.listen(3000, () => {
-  console.log("server is running on 3000 ❯❯❯❯");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
 });
